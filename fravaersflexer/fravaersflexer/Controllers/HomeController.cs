7d10e236067e051c5860ef0931e43dd64f63056e
gjellerhjort <mads.gjellerod@gmail.com>
2022-09-03 14:54:26 +0200

    added Sort table

--- fravaersflexer/fravaersflexer/Controllers/HomeController.cs
+++ fravaersflexer/fravaersflexer/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using fravaersflexer.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
+using System.Collections.Generic;
 using fravaersflexer.Data;
 
 namespace fravaersflexer.Controllers
@@ -16,19 +18,45 @@ namespace fravaersflexer.Controllers
             this.Context = _context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index(string sortOrder)
         {
+            ViewData["AbsenceSortParm"] = String.IsNullOrEmpty(sortOrder) ? "absence_desc" : "";
+            ViewData["AgeSortParm"] = sortOrder == "Age" ? "age_desc" : "Age";
+            ViewData["NameSortParm"] = sortOrder == "Name" ? "name_desc" : "Name";
+            var persons = from s in this.Context.Person select s;
             //List<Person> persons = (from person in this.Context.Person.Take(10) select person).ToList();
-            List<Person> persons = new List<Person> 
-            { 
-                new Person() { Age = 12, Name = "Mads", SchoolName = "College360", ClassName = "3X", EducationName = "HTX", AbsencePercentage = 20 },
-                new Person() { Age = 12, Name = "Morten", SchoolName = "Silkeborg Gym", ClassName = "2K", EducationName = "STX", AbsencePercentage = 3 },
-                new Person() { Age = 12, Name = "William", SchoolName = "Rudolf steiner skolen", ClassName = "3X", EducationName = "HHX", AbsencePercentage = 34 },
-                new Person() { Age = 12, Name = "Jens", SchoolName = "T.H Langs", ClassName = "1H", EducationName = "HF", AbsencePercentage = 65 },
-                new Person() { Age = 12, Name = "Kurt", SchoolName = "Silkeborg Gym", ClassName = "3J", EducationName = "STX", AbsencePercentage = 12 },
-                new Person() { Age = 12, Name = "Gorm", SchoolName = "College360", ClassName = "2XY", EducationName = "HTX", AbsencePercentage = 6 } 
-            };
-            return View(persons);
+            //List<Person> persons = new List<Person> 
+            //{ 
+            //    new Person() { Age = 12, Name = "Mads", SchoolName = "College360", ClassName = "3X", EducationName = "HTX", AbsencePercentage = 20 },
+            //    new Person() { Age = 12, Name = "Morten", SchoolName = "Silkeborg Gym", ClassName = "2K", EducationName = "STX", AbsencePercentage = 3 },
+            //    new Person() { Age = 12, Name = "William", SchoolName = "Rudolf steiner skolen", ClassName = "3X", EducationName = "HHX", AbsencePercentage = 34 },
+            //    new Person() { Age = 12, Name = "Jens", SchoolName = "T.H Langs", ClassName = "1H", EducationName = "HF", AbsencePercentage = 65 },
+            //    new Person() { Age = 12, Name = "Kurt", SchoolName = "Silkeborg Gym", ClassName = "3J", EducationName = "STX", AbsencePercentage = 12 },
+            //    new Person() { Age = 12, Name = "Gorm", SchoolName = "College360", ClassName = "2XY", EducationName = "HTX", AbsencePercentage = 6 } 
+            //};
+            switch (sortOrder)
+            {
+                case "absence_desc":
+                    persons = persons.OrderByDescending(s => s.Name);
+                    break;
+                case "Age":
+                    persons = persons.OrderBy(s => s.Age);
+                    break;
+                case "age_desc":
+                    persons = persons.OrderByDescending(s => s.Age);
+                    break;
+                case "Name":
+                    persons = persons.OrderBy(s => s.Name);
+                    break;
+                case "name_desc":
+                    persons = persons.OrderByDescending(s => s.Name);
+                    break;
+                default:
+                    persons = persons.OrderBy(s => s.AbsencePercentage);
+                    break;
+            }
+            //persons.Sort((x,y) => y.AbsencePercentage.CompareTo(x.AbsencePercentage));
+            return View(await persons.AsNoTracking().ToListAsync());
         }
 
         public IActionResult Privacy()
